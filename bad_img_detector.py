00d3bf911c9dcd9d102594dea2926e436cb662e6
Fredrik-hr <1119fredrik@gmail.com>
2025-04-29 12:54:01 +0200

    oppdatert meny

--- bad_img_detector.py
+++ bad_img_detector.py
@@ -12,27 +12,26 @@ from PIL import Image
 import warnings
 from tqdm import tqdm
 
-warnings.simplefilter('ignore', Image.DecompressionBombWarning) # If you dont trust the dataset you may whant to hash out this.
-Image.MAX_IMAGE_PIXELS = None # Sets the maximum number of pixelse to unlimited
+bad_img_lsit = [] # Stores bad images
 
-bad_images = [] # Stores bad images
- 
-user_input = input("Please enter the choosen file path:\n") # User input
-file_path = user_input # Variable to remember the user input
-
-# Get list of image files
-image_files = [f for f in listdir(file_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
-
-for filename in tqdm(image_files, desc="Checking images", unit="image"):
-    full_path = path.join(file_path, filename)
-    try:
-        img = Image.open(full_path) # open the image file
-        img.verify() # verify that it is, in fact an image
-    except (IOError, SyntaxError):
-        print('Bad file:', filename) # print out the names of corrupt files
-        bad_images.append(filename)
-
-with open("list_of_bad_images.txt", "w") as file:
-    for img in bad_images:
-        file.write(file_path+' '+img+'\n')
-    file.close()
+def bad_images(file_path):
+    
+    warnings.simplefilter('ignore', Image.DecompressionBombWarning) # If you dont trust the dataset you may whant to hash out this.
+    Image.MAX_IMAGE_PIXELS = None # Sets the maximum number of pixelse to unlimited
+    
+    # Get list of image files
+    image_files = [f for f in listdir(file_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
+    
+    for filename in tqdm(image_files, desc="Checking images", unit="image"):
+        full_path = path.join(file_path, filename)
+        try:
+            img = Image.open(full_path) # open the image file
+            img.verify() # verify that it is, in fact an image
+        except (IOError, SyntaxError):
+            print('Bad file:', filename) # print out the names of corrupt files
+            bad_img_lsit.append(filename)
+    
+    with open("list_of_bad_images.txt", "w") as file:
+        for img in bad_img_lsit:
+            file.write(file_path+' '+img+'\n')
+        file.close()
--- menu.py
+++ menu.py
@@ -5,49 +5,75 @@ Created on Tue Apr 29 12:10:12 2025
 @author: Fredrik
 """
 from preprocessing import resizer
-
+from bad_img_detector import bad_images
 
 # input_folder and output_folder will be the directory containing the images to resize
 def prepmain():
     print("Hello, you can preprocess you'r dataset of images here")
-    print("Would you like to look for 'bad images' or 'resize'? ")
-    choice = input().lower()
-    while choice == "resize":
-        print("1. prep train real")
-        print("2. prep train fake")
-        print("3. prep test real")
-        print("4. prep test fake")
-        print("5. end task")
-        choice = int(input())
-        
-        if choice == 1:
-            input_folder = 'C:/Users/Fredrik/MLEksamen/train/real'
-            output_folder = 'C:/Users/Fredrik/MLEksamen/resized_train/resized_real'
-            resizer(input_folder, output_folder)
-        elif choice == 2:
-            input_folder ='C:/Users/Fredrik/MLEksamen/train/fake'
-            output_folder = 'C:/Users/Fredrik/MLEksamen/resized_train/resized_fake'
-            resizer(input_folder, output_folder)
-        elif choice == 3:
-            input_folder = 'C:/Users/Fredrik/MLEksamen/test/real'
-            output_folder = 'C:/Users/Fredrik/MLEksamen/resized_test/resized_real'
-            resizer(input_folder, output_folder)
-        elif choice == 4:
-            input_folder = 'C:/Users/Fredrik/MLEksamen/test/fake'
-            output_folder = 'C:/Users/Fredrik/MLEksamen/resized_test/resized_fake'
-            resizer(input_folder, output_folder)
-        elif choice == 5:
+    while True:
+        print("Would you like to look for 'bad images' or 'resize'? ")
+        print("\nMain Menu:")
+        print("1. Look for bad images")
+        print("2. Resize images from the dataset")
+        print("0. Exit")
+        choice = input("Enter your choice as int: ")
+        if choice == "2":
+            while True:
+                print("1. prep train real")
+                print("2. prep train fake")
+                print("3. prep test real")
+                print("4. prep test fake")
+                print("5. end task")
+                sec_choice = input("Enter your choice as int: ")
+                
+                if sec_choice == "1":
+                    input_folder = 'C:/Users/Fredrik/MLEksamen/train/real'
+                    output_folder = 'C:/Users/Fredrik/MLEksamen/resized_train/resized_real'
+                    resizer(input_folder, output_folder)
+                elif sec_choice == "2":
+                    input_folder ='C:/Users/Fredrik/MLEksamen/train/fake'
+                    output_folder = 'C:/Users/Fredrik/MLEksamen/resized_train/resized_fake'
+                    resizer(input_folder, output_folder)
+                elif sec_choice == "3":
+                    input_folder = 'C:/Users/Fredrik/MLEksamen/test/real'
+                    output_folder = 'C:/Users/Fredrik/MLEksamen/resized_test/resized_real'
+                    resizer(input_folder, output_folder)
+                elif sec_choice == "4":
+                    input_folder = 'C:/Users/Fredrik/MLEksamen/test/fake'
+                    output_folder = 'C:/Users/Fredrik/MLEksamen/resized_test/resized_fake'
+                    resizer(input_folder, output_folder)
+                elif sec_choice == "5":
+                    break
+                else:
+                    print("Please enter a valid choice")
+                
+        elif choice == "1":
+            while True:
+                print("\nBad Images Menu:")
+                print("1. bad images train real")
+                print("2. bad images train fake")
+                print("3. bad images test real")
+                print("4. bad images test fake")
+                print("5. end task")
+                sec_choice = input("Enter your choice as int: ")
+                
+                if sec_choice == "1":
+                    file_path = 'C:/Users/Fredrik/MLEksamen/train/real'
+                    bad_images(file_path)
+                elif sec_choice == "2":
+                    file_path ='C:/Users/Fredrik/MLEksamen/train/fake'
+                    bad_images(file_path)
+                elif sec_choice == "3":
+                    file_path = 'C:/Users/Fredrik/MLEksamen/test/real'
+                    bad_images(file_path)
+                elif sec_choice == "4":
+                    file_path = 'C:/Users/Fredrik/MLEksamen/test/fake'
+                    bad_images(file_path)
+                elif sec_choice == "5":
+                    break
+                else:
+                    print("Please enter a valid choice")
+        elif choice == "0":
             break
-        else:
-            print("Please enter a valid choice")
-            
-    while choice == "bad images":
-        print("1. bad images train real")
-        print("2. bad images train fake")
-        print("3. bad images test real")
-        print("4. bad images test fake")
-        print("5. end task")
-        choice = int(input())
-        
         
 prepmain()
